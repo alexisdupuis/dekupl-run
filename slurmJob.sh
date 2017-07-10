f9f76fdce31e4812953cd8a76f7ee48e782a41c9
alexisdupuis <alexis.59.dupuis@gmail.com>
2017-07-10 14:19:22 +0200

    Delete slurmJob.sh

--- slurmJob.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#SBATCH --job-name=selectReads
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=24:00:00
-#SBATCH --mem=2048M
-
-./selectReads.sh /storage/ipl/collaboration/WGS 21
